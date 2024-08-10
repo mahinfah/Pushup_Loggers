9bd3db3656f34ca25758201ddaadf0fde0c68b98
mahinfah <mahinfah1234@gmail.com>
2024-08-10 15:39:17 -0800

    ok

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hi everyone \n hope all of you are doing goood'
+    return 'hi everyone \n hoood'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
