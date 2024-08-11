15f5326db8ef03e50d43462bcdc4e0be2ce5c640
mahinfah <mahinfah1234@gmail.com>
2024-08-11 01:12:07 -0800

    ok

--- app.py
+++ app.py
@@ -4,24 +4,29 @@ from datetime import datetime
 
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///file/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///file.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
 
 class Todo(db.Model): 
-   sno =db.Column(db.Integer, primary_key=True)
-  # title =db.Column(db.String(200), nullable=False)
-  # desc =db.Column(db.String(500), nullable=False)
-  # date_created =db.Column(db.DateTime,default=datetime.utcnow)
-   
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+with app.app_context(): 
+  db.create_all()
+
 def __repr__(self)->str:
     return f"{self.sno}- {self.title}"
 
 
 @app.route('/')
 def hello_world():
-    #return 'hi everyone \n hoood'
+    file = Todo(title="dsdsd",dec="asdas")
+    db.session.add(file)
+    db.session.commit()
     return render_template('index.html')
 
 if __name__ == "__main__":
