4bbee3f58de62f2dada120f891d08d68227f6b2b
mahinfah <mahinfah1234@gmail.com>
2024-08-11 00:18:18 -0800

    ok

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask ,render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
@@ -11,7 +11,7 @@ class Todo(db.Model):
    sno =db.Column(db.Integer, primary_key=True)
    title =db.Column(db.String(200), nullable=False)
    desc =db.Column(db.String(500), nullable=False)
-   date_created =db.Column(db.DateTime,default=datetime.utcnow)
+  # date_created =db.Column(db.DateTime,default=datetime.utcnow)
    
 def __repr__(self)->str:
     return f"{self.sno}- {self.title}"
