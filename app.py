3648da8883e8e11988f07a5f73f0eb6841a66f51
mahinfah <mahinfah1234@gmail.com>
2024-08-13 23:55:23 -0800

    ok

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask ,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
